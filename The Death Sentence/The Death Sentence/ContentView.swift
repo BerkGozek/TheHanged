87ac62e4eb169e693f8ea0f9ee8f1efd2bae19d7
Berk GOZEK <144159613+BerkGozek@users.noreply.github.com>
2023-10-05 09:24:52 +0300

    inicom

---
+++ The Death Sentence/The Death Sentence/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  The Death Sentence
+//
+//  Created by Berk Gozek on 05/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ The Death Sentence/The Death Sentence/The_Death_SentenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Death_SentenceApp.swift
+//  The Death Sentence
+//
+//  Created by Berk Gozek on 05/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct The_Death_SentenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
