4eba6aaa56160d4f6b74635324a5a3b3b881b970
Berk GOZEK <144159613+BerkGozek@users.noreply.github.com>
2023-10-26 16:20:30 +0300

    I hanged the man

--- The Death Sentence/The Death Sentence/ContentView.swift
+++ The Death Sentence/The Death Sentence/ContentView.swift
@@ -2,18 +2,43 @@
 //  ContentView.swift
 //  The Death Sentence
 //
-//  Created by Berk Gozek on 05/10/2023.
+//  Created by Berk Gozek on 05/50/2023.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing:0) {
+            Image("Head")
+                .resizable()
+                .frame(width: 50,height: 50)
+            HStack{
+                Image("Left")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .overlay(
+                        Image("Body")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .overlay(
+                                Image("Right")
+                                    .resizable()
+                                    .frame(width: 50, height: 50)
+                            )
+                    )
+            }//Body Fala
+            HStack(spacing : 0){
+                Image("Left")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .overlay(
+                        Image("Right")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                    )
+                
+            }//Bacak falan
         }
         .padding()
     }
